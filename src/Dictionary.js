c7de31f09321a8b3bfdc021e227964f52ee242cd
huxrce <its.erica.hu@gmail.com>
2026-03-11 17:25:53 +0100

    Added api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictitonary() {
   const [keyword, setKeyword] = useState(null);
 
-  function handleSubmit(event) {
+  function handleResult(response) {
+    return `${response.data}`;
+  }
+
+  function search(event) {
     event.prevent.Default();
-    alert(`Searching for the definition of ${keyword}`);
+    let apiKey = "etab4ae54eba8ab35o7f9e68e3e0f304";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResult);
   }
 
   function updateKeyword(event) {
@@ -15,7 +22,7 @@ export default function Dictitonary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={search}>
         <input type="search" className="user-input" onChange={updateKeyword} />
       </form>
     </div>
